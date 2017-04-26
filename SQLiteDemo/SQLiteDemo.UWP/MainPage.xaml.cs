39b1eee8b9b583754d743249f373d9ca4fd5bcec
Jesus Angulo <jesus.angulo@outlook.com>
2017-04-25 21:27:44 -0500

    Add project files.

---
+++ SQLiteDemo/SQLiteDemo.UWP/MainPage.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace SQLiteDemo.UWP
+{
+    public sealed partial class MainPage
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            LoadApplication(new SQLiteDemo.App());
+        }
+    }
+}
